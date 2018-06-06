b5a9645acda6e69c9f0acb7fb473a551a160c221
DanielFearn <dlhfearn@gmail.com>
2018-06-06 13:46:28 +0100

    Movement functionality added

--- towerofhanoi.js
+++ towerofhanoi.js
@@ -1,7 +1,13 @@
 var poles = [[4, 3, 2, 1, 0], [], []];
+var moves = 0;
+var screenWidth = 0;
 
-function start(){
-	
+var movingFrom = 3;
+
+function restart(){
+	poles = [[4, 3, 2, 1, 0], [], []];
+	moves = 0;
+	renderRings();
 }
 
 // tries to add a ring to a pole, returns true/false
@@ -27,34 +33,88 @@ function removeRing(pole){
 function renderRings(){
 	$('.toh-pole').empty();
 
+	$('#move-counter').html('Moves: '+moves);
+
 	for(var i = 0; i < poles.length; i++){
 		for(var j = 0; j < poles[i].length; j++){
 
 			var ringID = poles[i][j];
-			var ringWidth = ringID*20+80;
+			var ringWidth = ringID*(screenWidth/30)+56;
 			var ringBottom = j*36;
-			var ringLeft = 10-(ringID*10+40);
+			var ringLeft = 10-(ringID*(screenWidth/60)+28);
 			var ringStyles = `bottom: ${ringBottom}px; left: ${ringLeft}px; width: ${ringWidth}px;`;
 
 			var ringHtml = `<div class="toh-ring" id="toh-ring-${ringID}" style="${ringStyles}"></div>`;
 			$('#toh-pole-'+i).append(ringHtml);
 		}
+
+		$('#toh-pole-'+i).append('<div class="movehere">Move Here</div>');
+		$('.movehere').hide();
 	}
 }
 
 // move a ring from one pole to another
 function moveRing(from, to){
-	if(from == to){
+	if(from == to || poles[from].length == 0){
 		return false;
 	}else{
 		var ring = poles[from][poles[from].length-1];
 		if(addRing(to, ring)){
 			removeRing(from);
+			moves++;
 			renderRings();
+			checkComplete();
+			return true;
+		}else{
+			return false;
 		}
 	}
 }
 
+// activate move
+function activateMove(pole){
+	if(movingFrom == 3){
+		movingFrom = pole;
+	}else{
+		$('.movehere').hide();
+		moveRing(movingFrom, pole);
+		$('.movehere').hide();
+		movingFrom = 3;
+	}
+}
+
+// checks if the puzzle is complete
+function checkComplete(){
+	if(JSON.stringify(poles[2]) == '[4,3,2,1,0]'){
+		setTimeout(displayCode, 100);
+	}
+}
+
+function displayCode(){
+	$('#code').fadeIn(1000);
+}
+
 $(document).ready(function(){
+	screenWidth = $('#toh-cont').width();
+	console.log(screenWidth);
 	renderRings();
+
+	$('#code').hide();
+
+
+	$('#toh-pole-0').click(function(){
+		$('#toh-pole-1 .movehere').show();
+		$('#toh-pole-2 .movehere').show();
+		activateMove(0);
+	});
+	$('#toh-pole-1').click(function(){
+		$('#toh-pole-0 .movehere').show();
+		$('#toh-pole-2 .movehere').show();
+		activateMove(1);
+	});
+	$('#toh-pole-2').click(function(){
+		$('#toh-pole-0 .movehere').show();
+		$('#toh-pole-1 .movehere').show();
+		activateMove(2);
+	});
 });
